2a37475a65d895a25396e7d76173b62786db97c5
TanneA <91456442+albin-tnn@users.noreply.github.com>
2021-11-18 13:06:40 +0000

    Choix du nbre de questions

--- main.py
+++ main.py
@@ -24,21 +24,19 @@ f.seek(0)
 
 reader = csv.reader(f, delimiter=',')
 
-q = 0 
-while q < 3 : # Alterne entre question du bot, entrée de l'utilisateur et réponse du bot
-  q +=1       # Limite le nombre de questions à trois
+nb_q = int(input("Entrez la durée de la manche : "))
+
+for q in range(1, nb_q+1) : # Alterne entre question du bot, entrée de l'utilisateur et réponse du bot
   rand_nb = random.randint(0, nl-1)
   current_row = []
   for i, row in enumerate(reader):
     if i == rand_nb:
       current_row = row
   
-  print (f'Question {q}/3\n')
+  print (f'Question {q}/{nb_q}\n')
   print(f"{current_row[0]}\n{current_row[1]}\t{current_row[2]}\t{current_row[3]}\t{current_row[4]}\n")    
-# peut être que faut relire le fichier depuis le début ? # bien vu haha # C'est Rémi (l'assistant) qui a remarqué :) 
   user_input = str(input("Votre réponse : "))
   print(f'\nJean-Pierre Foucault ==> {bot_answer(user_input)} \n')
   f.seek(0)
 
 print (f'La partie est terminée !\nVotre score final est de {nb_points} points !')
-
